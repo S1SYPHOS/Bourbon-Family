8cbc2f1575c215febbeab05b2b118466c01e8798
M. Folkers <hello@twobrain.io>
2017-10-10 21:33:56 +0200

    Fixed update script

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 wget https://raw.githubusercontent.com/necolas/normalize.css/master/normalize.css -O src/utility/_normalize.scss
 rm -r src/vendor && rm -r src/base
 mkdir -p test && cd test
-bourbon install && neat install && bitters install
+../bin/bourbon install && ../bin/neat install && ../bin/bitters install
 mkdir -p ../src/vendor && mv bourbon ../src/vendor/ && mv neat ../src/vendor/
 mv base/_variables.scss ../src/utility/_settings.scss
 rm base/_base.scss
